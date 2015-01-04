eeb128683bf454b3e990a4ba7e0661e54e637d68
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2015-01-04 13:53:30 -0500

    Provide a "connect" function.
    It's three lines but it's really nice not having to remember what
    exactly those three lines are every time.

--- catdex/db/__init__.py
+++ catdex/db/__init__.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column
+from sqlalchemy import Column, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import Integer, Unicode
 
 DBSession = sessionmaker()
 TableBase = declarative_base()
+
+def connect(uri):
+    """Connect to the db and return a session."""
+
+    engine = create_engine(uri)
+    DBSession.configure(bind=engine)
+    return DBSession()
 
 class Pokemon(TableBase):
     """A species of Pokémon."""
