968885b3b6544943fdf129ef224b7d2fe1a326f4
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2015-01-04 19:59:40 -0500

    Add the rest of the Pokémon and their forms.
    I think a dual primary key for pokemon_forms is nicer, but I may change
    it if it turns out to be unwieldy.
    
    A lot of the form orders and default forms I went with don't match up
    with what the games say:
    
    - Plain old Cosplay Pikachu goes after all its costumes internally, but
      that seems unintuitive to me
    - Arceus's internal order matches the types' internal order, but I'm
      going with the semi-official Normal-Fire-Water order for both (I may
      end up changing my mind though)
    - The games treat Icy Snow Vivillon (i.e. the first form) as the
      default, but Nintendo treats Meadow Vivillon as the default, so I
      went with Meadow (though it's still seventh, despite being default)
    - Internally, Furfrou's trims go Natural, Heart, Star, Diamond,
      Debutante, Matron, Dandy, La Reine, Kabuki, Pharaoh; however, the
      order you unlock them in, and the order they're presented to the
      player when grooming Furfrou, has the middle three and last three
      swapped (assuming nothing changed in OR/AS), which is what I went
      with
    - The games treat male as default, but I'm treating female as default;
      Meowstic reflects this
    - The games put Average Pumpkaboo & Gourgeist first; I sort them by
      size (though Average is still the default, despite being second)
    - I'm going with Active Xerneas as default for the same reason as
      Vivillon (though I know that reasoning is a little weaker here given
      that you actually can argue that Xerneas does default to a particular
      form)
    
    Also, this doesn't include Spiky-eared Pichu or ??? Arceus (yet).

--- catdex/db/__init__.py
+++ catdex/db/__init__.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, create_engine
+from sqlalchemy import Column, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.types import Integer, Unicode
+from sqlalchemy.types import Boolean, Integer, Unicode
+
 
 DBSession = sessionmaker()
 TableBase = declarative_base()
@@ -13,10 +14,27 @@ def connect(uri):
     DBSession.configure(bind=engine)
     return DBSession()
 
+
+### Tables
+
 class Pokemon(TableBase):
-    """A species of Pokémon."""
+    """One of the 721 (as of Generation VI) species of Pokémon."""
 
     __tablename__ = 'pokemon'
 
     id = Column(Integer, primary_key=True)
-    identifier = Column(Unicode, nullable=False)
+    identifier = Column(Unicode, unique=True, nullable=False)
+
+class PokemonForm(TableBase):
+    """A specific form of a Pokémon.
+
+    Pokémon that don't have multiple forms still have a row in this table for
+    their single form.
+    """
+
+    __tablename__ = 'pokemon_forms'
+
+    pokemon_id = Column(Integer, ForeignKey('pokemon.id'), primary_key=True)
+    form_id = Column(Integer, primary_key=True)
+    identifier = Column(Unicode, unique=True, nullable=False)
+    is_default = Column(Boolean, nullable=False)
