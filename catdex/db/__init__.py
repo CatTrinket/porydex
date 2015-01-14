97f2c5860115d487ab1df7ac49a847da27e0af94
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2015-01-14 00:09:44 -0500

    Add games and generations.
    Identifiers are a little redundant for generations but they're
    consistent.  I want to have identifiers for every, like, unit concept.
    
    I'm not 100% sure what to do with game IDs... I sorted them in release
    order (i.e. FR/LG between R/S and E) because I don't know whether X/Y
    are going to get a third version or a pair of sequels like B/W did and
    I want to keep IDs consistent.  I also didn't add Colosseum and XD
    though, and when I do ID isn't going to represent sort order *or*
    release order...
    
    I guess really all I want is for IDs to be *tidy*, and that's okay.

--- catdex/db/__init__.py
+++ catdex/db/__init__.py
@@ -25,6 +25,29 @@ class Ability(TableBase):
     id = Column(Integer, primary_key=True)
     identifier = Column(Unicode, unique=True, nullable=False)
 
+class Game(TableBase):
+    """One of the main-series Pokémon games."""
+
+    __tablename__ = 'games'
+
+    id = Column(Integer, primary_key=True)
+    identifier = Column(Unicode, unique=True, nullable=False)
+    generation_id = Column(Integer, ForeignKey('generations.id'),
+                           nullable=False)
+
+class Generation(TableBase):
+    """One iteration of the Pokémon series.
+
+    For example, the original 151 Pokémon and the games Red, Blue, and Yellow
+    all belong to Generation I.  The next 100 Pokémon and Gold, Silver, and
+    Crystal belong to Generation II, and so on.
+    """
+
+    __tablename__ = 'generations'
+
+    id = Column(Integer, primary_key=True)
+    identifier = Column(Unicode, unique=True, nullable=False)
+
 class Move(TableBase):
     """A move (e.g. Tackle, Growl)."""
 
