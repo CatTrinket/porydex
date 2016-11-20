b89e5b4ff2d2a8badda42cc057af9c07630ec785
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2016-11-20 15:51:17 -0500

    Add generation types, including ???.

--- porydex/db/schema/type.py
+++ porydex/db/schema/type.py
@@ -1,6 +1,8 @@
 import sqlalchemy as sa
+import sqlalchemy.orm
 
 from ..core import TableBase
+from ..util import attr_ordereddict_collection
 from .pokemon import pokemon_form_key, generation_pokemon_form_key
 
 
@@ -11,6 +13,12 @@ class Type(TableBase):
 
     id = sa.Column(sa.Integer, primary_key=True)
     identifier = sa.Column(sa.Unicode, unique=True, nullable=False)
+
+    _by_generation = sa.orm.relationship(
+        'GenerationType',
+        collection_class=attr_ordereddict_collection('generation_id'),
+        order_by='GenerationType.generation_id'
+    )
 
 class PokemonType(TableBase):
     """One of a Pokémon form's types in a particular generation."""
@@ -24,3 +32,13 @@ class PokemonType(TableBase):
     form_id = sa.Column(sa.Integer, primary_key=True, autoincrement=False)
     slot = sa.Column(sa.Integer, primary_key=True, autoincrement=False)
     type_id = sa.Column(sa.Integer, sa.ForeignKey('types.id'))
+
+class GenerationType(TableBase):
+    """A generation that a type appears in."""
+
+    __tablename__ = 'generation_types'
+
+    generation_id = sa.Column(sa.Integer, sa.ForeignKey('generations.id'),
+                              primary_key=True)
+    type_id = sa.Column(sa.Integer, sa.ForeignKey('types.id'),
+                        primary_key=True)
