77219bb83b98001bb54f40560db55c2742617a0c
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2017-08-29 01:11:01 -0400

    Add names and generations to moves and abilities.
    I'm not going to worry about handling names per gen for now.

--- porydex/db/schema/ability.py
+++ porydex/db/schema/ability.py
@@ -1,12 +1,37 @@
 import sqlalchemy as sa
 
+from .language import ByLanguage
 from ..core import TableBase
+from ..util import ExistsByGeneration
 
 
-class Ability(TableBase):
+class Ability(TableBase, ExistsByGeneration, ByLanguage):
     """An ability (e.g. Overgrow, Blaze, Torrent)."""
 
     __tablename__ = 'abilities'
+    _by_generation_class_name = 'GenerationAbility'
+    _by_language_class_name = 'AbilityName'
 
     id = sa.Column(sa.Integer, primary_key=True)
     identifier = sa.Column(sa.Unicode, unique=True, nullable=False)
+
+class AbilityName(TableBase):
+    """An ability's name in a particular language."""
+
+    __tablename__ = 'ability_names'
+
+    language_id = sa.Column(sa.Integer, sa.ForeignKey('languages.id'),
+                            primary_key=True)
+    ability_id = sa.Column(sa.Integer, sa.ForeignKey('abilities.id'),
+                           primary_key=True)
+    name = sa.Column(sa.Text, nullable=False)
+
+class GenerationAbility(TableBase):
+    """A generation that an ability appears in."""
+
+    __tablename__ = 'generation_abilities'
+
+    generation_id = sa.Column(sa.Integer, sa.ForeignKey('generations.id'),
+                              primary_key=True)
+    ability_id = sa.Column(sa.Integer, sa.ForeignKey('abilities.id'),
+                           primary_key=True)
--- porydex/db/schema/move.py
+++ porydex/db/schema/move.py
@@ -1,12 +1,37 @@
 import sqlalchemy as sa
 
+from .language import ByLanguage
 from ..core import TableBase
+from ..util import ExistsByGeneration
 
 
-class Move(TableBase):
+class Move(TableBase, ExistsByGeneration, ByLanguage):
     """A move (e.g. Tackle, Growl)."""
 
     __tablename__ = 'moves'
+    _by_generation_class_name = 'GenerationMove'
+    _by_language_class_name = 'MoveName'
 
     id = sa.Column(sa.Integer, primary_key=True)
     identifier = sa.Column(sa.Unicode, unique=True, nullable=False)
+
+class MoveName(TableBase):
+    """A move's name in a particular language."""
+
+    __tablename__ = 'move_names'
+
+    language_id = sa.Column(sa.Integer, sa.ForeignKey('languages.id'),
+                            primary_key=True)
+    move_id = sa.Column(sa.Integer, sa.ForeignKey('moves.id'),
+                        primary_key=True)
+    name = sa.Column(sa.Text, nullable=False)
+
+class GenerationMove(TableBase):
+    """A generation that a move appears in."""
+
+    __tablename__ = 'generation_moves'
+
+    generation_id = sa.Column(sa.Integer, sa.ForeignKey('generations.id'),
+                              primary_key=True)
+    move_id = sa.Column(sa.Integer, sa.ForeignKey('moves.id'),
+                        primary_key=True)
