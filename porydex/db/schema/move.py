fc8fb6278b0979876c52813526581bec1ceedab7
Trinket <trinket@catseyemarble.com>
2020-07-04 15:52:02 -0400

    Add TM/HM/TR numbers.
    - For Gen 1, Gen 2, and RSE, I found the TM/HM list in the source code
      for pret's disassemblies, copied it into a text editor, and did some
      regexes to turn it into SQL.
    
    - For DPPt, X/Y, Gen 7, and Let's Go, I ripped the list myself.
    
    - For FR/LG, HG/SS, and OR/AS, I copied the TMs/HMs from earlier games
      in the gen, and made the few required tweaks manually.
    
    - For Gen 5 and 8, I couldn't find the TM list, so instead I wrote a
      script to compare item flavour and move flavour and match TMs with
      moves that way.
    
      This worked pretty well for Gen 8; a few moves turned out to have the
      same text as other moves, so I had it prompt me to choose one in those
      cases.  In Gen 5, quite a few moves (like 20-30?) had slightly
      different item vs move text, so I had it print the item text so I
      could enter a move manually.  Then I diffed my entire list against
      Bulbapedia's to be sure.
    
      So all in all I spent a few hours on those gens compared to like
      twenty minutes on all the others combined, and I kind of wish I had
      just copied Bulbapedia's list to begin with, lol.

--- porydex/db/schema/move.py
+++ porydex/db/schema/move.py
@@ -1,5 +1,8 @@
+import enum
+
 import sqlalchemy as sa
 
+from .game import Game
 from .language import ByLanguage
 from ..core import TableBase
 from ..util import ExistsByGeneration
@@ -15,6 +18,7 @@ class Move(TableBase, ExistsByGeneration, ByLanguage):
     id = sa.Column(sa.Integer, primary_key=True)
     identifier = sa.Column(sa.Unicode, unique=True, nullable=False)
 
+
 class MoveName(TableBase):
     """A move's name in a particular language."""
 
@@ -26,6 +30,7 @@ class MoveName(TableBase):
                         primary_key=True)
     name = sa.Column(sa.Text, nullable=False)
 
+
 class GenerationMove(TableBase):
     """A generation that a move appears in."""
 
@@ -35,3 +40,27 @@ class GenerationMove(TableBase):
                               primary_key=True)
     move_id = sa.Column(sa.Integer, sa.ForeignKey('moves.id'),
                         primary_key=True)
+
+
+class MoveMachineType(enum.Enum):
+    """An enum for the three types of move-teaching machine (TM, HM, TR)."""
+
+    tm = enum.auto()
+    hm = enum.auto()
+    tr = enum.auto()
+
+    def __str__(self):
+        return self.name
+
+
+class MoveMachine(TableBase):
+    """A move taught by a TM/HM/TR in a particular game."""
+
+    __tablename__ = 'move_machines'
+
+    game_id = sa.Column(sa.ForeignKey(Game.id), primary_key=True)
+    move_id = sa.Column(sa.ForeignKey(Move.id), primary_key=True)
+    machine_type = sa.Column(sa.Enum(MoveMachineType), nullable=False)
+    number = sa.Column(sa.Integer, nullable=False)
+
+    __table_args__ = (sa.UniqueConstraint(game_id, machine_type, number),)
